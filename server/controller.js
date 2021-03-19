3396075d000eb848df1cabdce72b073a6efdb60a
Jake Carr <jcarr0157@gmail.com>
2021-03-18 17:25:46 -0700

    set up controller and routes, tested all on postman, db helpers next

--- server/controller.js
+++ server/controller.js
@@ -1,9 +1,46 @@
 // require db helpers
 
 const controller = {
-  get: (req, res) => {
-    // params page (default 1), count (default 5), product_id
-    res.send('greetings')
+  addQuestion: (req, res) => {
+    // req.body params: body, name, email, product_id
+    // status 201
+    res.send('add question')
+  },
+  getQuestionsByProductId: (req, res) => {
+    // params product_id, page (default 1), count (default 5),
+    // status 200
+    res.send('questions by product id')
+  },
+  getAnswersByQuestionId: (req, res) => {
+    // params product_id, page (default 1), count (default 5),
+    // status 200
+    res.send('answers by question id')
+  },
+  addAnswer: (req, res) => {
+    // params question_id
+    // req.body params: body, name, email, photos
+    // status 201
+    res.send('add answer')
+  },
+  helpfulQuestion: (req, res) => {
+    // params question_id
+    // status 204
+    res.send('helpful question')
+  },
+  reportQuestion: (req, res) => {
+    // params question_id
+    // status 204
+    res.send('reported question')
+  },
+  helpfulAnswer: (req, res) => {
+    // params question_id
+    // status 204
+    res.send('helpful answer')
+  },
+  reportAnswer: (req, res) => {
+    // params question_id
+    // status 204
+    res.send('reported answer')
   }
 }
 
--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,30 @@
 const router = require('express').Router();
 const controller = require('./controller.js');
 
+// /qa/
+
 router
-  .route('/test')
-    .get(controller.get)
-  // .route('/questions/:product_id')
-  //   .get(controller.get)
+  .route('/questions')
+    .post(controller.addQuestion) // add a question, which will have a product_id property
+router
+  .route('/questions/:product_id')
+    .get(controller.getQuestionsByProductId) // retrieve a list of questions
+router
+  .route('/questions/:question_id/answers')
+    .get(controller.getAnswersByQuestionId) // retrieves a list of answers
+    .post(controller.addAnswer) // post new answer to a question id
+router
+  .route('/questions/:question_id/helpful')
+    .put(controller.helpfulQuestion) // mark a question as helpful
+router
+  .route('/questions/:question_id/report')
+    .put(controller.reportQuestion) // report a question
+router
+  .route('/answers/:answer_id/helpful')
+    .put(controller.helpfulAnswer) // mark an answer as helpful
+router
+  .route('/answers/:answer_id/report')
+    .put(controller.reportAnswer) // report an answer
+
 
 module.exports = router;
