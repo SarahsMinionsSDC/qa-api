05b1912137a6ad9550ca32b110c14129f5a3550a
Jake Carr <jcarr0157@gmail.com>
2021-03-24 14:26:48 -0700

    change verification for nginx instance on loaderio

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use('/qa', router)
 
-app.get('/loaderio-4dde61e2dfcf9bb782bc6ce626d88010', (req, res) => {
-  res.send('loaderio-4dde61e2dfcf9bb782bc6ce626d88010')
+app.get('/loaderio-9893bd0bc1dd3e4997d8ae8cc258995c', (req, res) => {
+  res.send('loaderio-9893bd0bc1dd3e4997d8ae8cc258995c')
 })
 
 app.listen(port, () => {
